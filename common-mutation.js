a911dd8a7743ce4001d6e5ee55bccaaaab4b8f54
Dhaval Ladani <dhaval@techerudite.com>
2024-07-01 10:58:11 +0530

    added common mutation

---
+++ common-mutation.js
@@ -0,0 +1,26 @@
+// you need to replace - mutation_name_ & MutationName accross this function
+
+const MUTATION_NAME = gql`MUTATION_PLACEHOLDER`;
+
+export const MutationNameAction = () => {
+    const [mutateFn, mutation_name_Status] = useMutation(MUTATION_NAME);
+
+    const initMutationName = (data, successCallback, errorCallback) => {
+        mutateFn({
+            variables: data,
+        })
+            .then((responseData) => {
+                const resData = responseData.data.mutation_name_;
+                if (resData.success) {
+                    successCallback && successCallback(resData.data);
+                } else {
+                    errorCallback && errorCallback(responseData);
+                }
+            })
+            .catch((err) => {
+                errorCallback && errorCallback(err);
+            });
+    };
+
+    return { mutation_name_Status, initMutationName };
+};
