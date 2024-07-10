ff5d0eb7e1393913dc8795a03818fd4649313dec
techerudite-dhaval-l <167055471+techerudite-dhaval-l@users.noreply.github.com>
2024-07-10 17:49:00 +0530

    Create faces-cookies.js

---
+++ faces-cookies.js
@@ -0,0 +1,8 @@
+function getCookie(name = 'faces_access_token') {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+    return null;
+}
+
+getCookie();
